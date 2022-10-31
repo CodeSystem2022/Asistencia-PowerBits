bc0964a6d034549babac8f094a2e6438e32b4149
juaniM4c <112594646+juaniM4c@users.noreply.github.com>
2022-10-31 17:08:20 -0300

    Asistencia Juani

--- Asistencia Octubre.java
+++ Asistencia Octubre.java
@@ -20,3 +20,32 @@ public class Ciclos11 {
         JOptionPane.showMessageDialog(null, "El producto de los numero impares es: "+producto);
     }   
 }
+
+
+// Machuca Juan 
+
+package ciclos12;
+
+import java.util.Scanner;
+
+//import javax.swing.JOptionPane;
+
+public class Ciclos12 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        long factorial = 1; 
+        
+        System.out.println("Digite un numero: ");
+        int numero = Integer.parseInt(entrada.nextLine());
+        //int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        for(int i = 1; i <= numero; i++){
+            factorial *= i;
+        }
+        
+        System.out.println("\nEl factorial del numero ingresado es: "+factorial);
+        //JOptionPane.showMessageDialog(null, "El factorial del numero ingresado es: "+factorial);
+        
+        
+    }
+}
