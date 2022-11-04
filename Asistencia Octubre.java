971077aab9e9c271d0e21dc3ee1ca5f54519fcb8
Antonella arraez <112026724+antonellaarraez@users.noreply.github.com>
2022-11-04 12:37:27 -0300

    Update Asistencia Octubre.java
    Asistencia  Octubre Arraez Antonella

--- Asistencia Octubre.java
+++ Asistencia Octubre.java
@@ -146,3 +146,35 @@ public class PersonaPrueba {
          System.out.println("El nuevo booleano de persona2 es: "+persona2.isEliminado()); 
     }
 }
+
+//Arraez Antonella
+//Ejercicio con contexto estático
+package test;
+
+import domain.Persona;
+
+public class PersonaPrueba {
+    private int contador;
+    
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Ariel");
+        System.out.println("persona1 = " + persona1);
+        Persona persona2 = new Persona("Naty");
+        System.out.println("persona2 = " + persona2);
+        imprimir(persona1);
+        imprimir(persona2);
+        //this.contador = 10;//No se puede referenciar desde un contexto estatico
+        PersonaPrueba personaP1 = new PersonaPrueba();
+        System.out.println(personaP1.getContador());
+    }
+    
+    static public void imprimir(Persona persona){
+        System.out.println("persona = " + persona);
+    }
+    
+    public int getContador(){
+        imprimir(new Persona("Liliana"));
+        return this.contador;
+    }
+}
+
